170df5e52e173386dd1c2c318bee8fea1cd9055c
manhdung0420 <dungdmph49130@gmail.com>
2025-05-25 00:05:10 +0700

    CRUD order

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model
+{
+
+    use HasFactory;
+
+    protected $table = 'order_items';
+    protected $fillable = [
+        'order_id', 'product_variant_id', 'quantity', 'price_at_time', 'total_price',
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function productVariant()
+    {
+        return $this->belongsTo(ProductVariant::class, 'product_variant_id');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BarberController;
 use App\Http\Controllers\BranchController;
@@ -29,6 +30,9 @@ Route::middleware(['auth', 'role'])->prefix('admin')->group(function () {
 
     // Hiển thị giao diện danh sách Thợ cắt tóc
     Route::resource('barbers', BarberController::class);
+
+    Route::resource('orders', OrderController::class);
+
     
     // ==== Dịch vụ ====
     Route::get('/services', [ServiceController::class, 'index'])->name('admin.services.index');
