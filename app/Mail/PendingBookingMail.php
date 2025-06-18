a444eb98e13a278837caca54b59048096ff8a076
trandat <dattlph49133@gmail.com>
2025-06-18 18:11:22 +0700

    fix notification

---
+++ app/Mail/PendingBookingMail.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Appointment;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class PendingBookingMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $appointment;
+
+    public function __construct(Appointment $appointment)
+    {
+        $this->appointment = $appointment;
+    }
+
+    public function build()
+    {
+        return $this->subject('Lịch hẹn của bạn đang chờ xác nhận')
+            ->view('emails.pending_booking')
+            ->with(['appointment' => $this->appointment]);
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.pending_booking',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
---
+++ resources/views/emails/pending_booking.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Cảm ơn ban đã đặt lịch</title>
+</head>
+
+<body>
+    <h1>Xin chào {{ $appointment->name }},</h1>
+    <p>Lịch hẹn của bạn đã được gửi và đang chờ xác nhận. Chúng tôi sẽ thông báo khi lịch được phê duyệt.</p>
+    <p>Trân trọng,<br>Đội ngũ hỗ trợ</p>
+</body>
+
+</html>
