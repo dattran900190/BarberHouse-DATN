49ff52221c189d63c99a51cd608e6dfcc75aecad
manhdung0420 <dungdmph49130@gmail.com>
2025-07-02 16:07:26 +0700

    test hoan tien

--- database/migrations/2025_05_12_082431_add_foreign_keys_to_refund_requests_table.php
+++ database/migrations/2025_05_12_082431_add_foreign_keys_to_refund_requests_table.php
@@ -8,29 +8,23 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('refund_requests', function (Blueprint $table) {
-            $table->foreign(['user_id'], 'refund_requests_ibfk_1')
-                ->references(['id'])->on('users')
-                ->onUpdate('no action')->onDelete('no action');
+            $table->foreign('user_id', 'refund_requests_user_id_fk')
+                ->references('id')->on('users')
+                ->onUpdate('no action')
+                ->onDelete('no action');
 
-            $table->foreign(['order_id'], 'refund_requests_ibfk_2')
-                ->references(['id'])->on('orders')
-                ->onUpdate('no action')->onDelete('no action');
-
-            // $table->foreign(['order_id'], 'refund_requests_ibfk_3')
-            //     ->references(['id'])->on('orders')
-            //     ->onUpdate('no action')->onDelete('no action');
-
-
+            $table->foreign('order_id', 'refund_requests_order_id_fk')
+                ->references('id')->on('orders')
+                ->onUpdate('no action')
+                ->onDelete('no action');
         });
     }
 
     public function down(): void
     {
         Schema::table('refund_requests', function (Blueprint $table) {
-            $table->dropForeign('refund_requests_ibfk_1');
-            $table->dropForeign('refund_requests_ibfk_2');
-            // $table->dropForeign('refund_requests_ibfk_3');
-            
+            $table->dropForeign('refund_requests_user_id_fk');
+            $table->dropForeign('refund_requests_order_id_fk');
         });
     }
 };
